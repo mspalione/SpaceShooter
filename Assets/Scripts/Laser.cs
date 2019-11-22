bf9dddbf44104d6f0ad05ccfbba686f7cb08c3e6
Melissa Spalione <melissaspalione86@gmail.com>
2019-11-21 23:51:15 -0500

    Upload Baseline project

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    //speed variable of 8
+    [SerializeField]
+    private float _speed = 8.0f;
+    [SerializeField]
+    private GameObject _laser;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var y = transform.position.y;
+        //translate laser up
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if(y > 8f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take current position and assign new position (0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        // space key to fire laser
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire) { FireLaser(); }
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        var pos = transform.position;
+        Vector3 p = new Vector3(pos.x, pos.y, 0);
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);       
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser()
+    {
+            _canFire = Time.time + _fireRate;
+            var p = transform.position + new Vector3(0, .8f, 0);
+            Instantiate(_laserPrefab, p, Quaternion.identity);
+    }
+}
